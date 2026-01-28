7d52b384e8ee5dc0c5da6d6ee56fe8f2b4bcd1f1
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2026-01-28 16:58:54 +0900

    Day13 퀵 정렬 구현

--- Program4/Algorithm/main.cpp
+++ Program4/Algorithm/main.cpp
@@ -191,6 +191,72 @@ void MergeSort(vector<int>& v, int left, int right)
 
 // quick sort. 
 
+// 버선삽 - N^2
+// 힙, Merge, quick
+
+// 퀵소트
+// quick - 빠르다?	- merge비슷하다.  - [ ] 나누기.
+// merge -> middle 
+// 피벗 빠르게 선택했다. 
+
+// pivot
+
+// [5] | [1][3][7][9][2][4][6][8]
+// P	 	 	l		 h	     	     pivot 어느 위치에 있으면 가장 어울릴까?	<-[5]->
+//       [1][3][4][9][2][7][6][8]
+//                 l  h  
+//      [2][1][3][4][5][9][7][6][8]                  
+// 				   h  l
+//      [2][1][3][4][5][9][7][6][8]      
+// 1> low high low->   <-high  pivot값
+// // v[low] <= p        v[high] >= p
+// 2> low index값(2)  <  high(5)	  swap
+// 3>  swap(v[left],v[h])
+
+
+// Heap vs Merge vs Q [Sort]
+// nlogn              n^2, nlogn
+// nlogn 시간을 갖는다.
+// Q-sort 시간이 쪼금 효율적이지 않다. 그렇다면 무엇이 더 좋을까?
+// 
+
+int  Partition(vector<int>& v, int left, int right)
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+
+	// 횟수가 많아지겠다 [(N) - logN  *  n]	 n^2. n logn
+	while (low <= high)
+	{
+		while (low <= right && pivot >= v[low])	 // 배열 범위  && 피벗 비교	
+			low++;
+		
+		while (high >= left + 1 && pivot <= v[high])   // 배열 범위  && 피벗 비교	
+			high--;
+
+		if (low < high)
+			swap(v[low], v[high]);
+	}
+
+	swap(v[left], v[high]);
+	return high;             // pivot이 위치해야할 인덱스 값.               
+}
+
+void QuickSort1(vector<int>& v, int left, int right)
+{
+	if (left > right)
+		return;
+
+	int pivot = Partition(v, left, right);
+	QuickSort1(v, left, pivot-1);  // 왼쪽 파트
+	QuickSort1(v, pivot+1, right); // 오른쪽 파트
+}
+
+// 탐색. N      - 전수 조사 N
+// 1 ~ 30  15. - 키 정렬.
+// nlogn * logn = nlog(n제곱)
+
 int main()
 {
 	vector<int> v{ 4,3,1,9,7 };
@@ -205,7 +271,16 @@ int main()
 	//HeapSort(v);
 	//MergeSort(v, 0, v.size() - 1);
 
-	sort(v.begin(), v.end());
+	//QuickSort1(v, 0, v.size() - 1);	   // [1]  1 0
+
+	sort(v.begin(), v.end()); // ----
+	// 더 효율적인 방법이 있다고. 
+	// 연관 컨테이너 - 탐색	
+	// 추가.logN <<<< 로딩. ?? 로딩하는 시간이 너무 오래걸려서 화가 너무난다. <<
+	//                         실제 게임중에 렉이거려서 내 캐릭터가 죽었다.  <<
+	// vector<>
+	// 렉. ?.
+	// 탐색 시간이 빠릅니다. logN.
 
 	for (const auto& a : v)
 	{
