97c9fa068882180133171e12cf729d882256fa00
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2026-01-29 17:01:00 +0900

    Day14 HashTable 개요

--- Program4/Algorithm/main.cpp
+++ Program4/Algorithm/main.cpp
@@ -13,146 +13,137 @@
 #include <Windows.h>
 using namespace std;
 
-// 탐색에서 더 효율적인 컨테이너.
-// 트리 - 계층을 갖는 vertex edge
-// 이진트리 (BST). 이진 탐색
-// 트리를 어떤 방식으로 순회 - [BFS DFS]
-// [Pre, In, Post] Order
+// Hash Table
 
+// 비용. BFS -> mindistance N^2 젤작은거 찾자. - 전수 조사
+// sort() -> nlogn
+// PQ -> O(1) - logn
 
-//             insert(10)  
-//     insert(5)       (15)
-//          root(10)
-//		  5		  15
-//     3    7   
-//         6
+// [0] vector
+// [1] map vs hash table  
+// map -> logn
 
-void SetCursorPosition(int x, int y)
+// table - 시간을 위해 공간을 사용했다.
+// 어디까지 내줄건데?  
+
+void Table()
 {
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD  pos = { static_cast<SHORT>(x),static_cast<SHORT>(y) };
-	::SetConsoleCursorPosition(output, pos);
+	struct User
+	{
+		int id = 0;
+		int value;
+	};
+
+	vector<User> users;	 
+	users.resize(1000);
+	
+	users[999] = User{ 999, 999 * 10 };
+
+	// Q 999번 아이디의 유저가 가지고 있는 값을 출력하세요.
+
+	cout << "유저가 가지고 있는 값 : " << users[999].value << endl;
 }
 
-struct Node
+// hash	- 
+// hashing?
+// 너무 많다. => 1억 ( ) => 1A  + 
+
+
+void Hash()
 {
-	Node* parent = nullptr;
-	Node* left = nullptr;
-	Node* right = nullptr;
-	int key = {};
-};
+	struct User
+	{
+		int id = 0;
+		string name;
+	};
+
+	vector<User> users;
+
+	users.resize(1000);
+
+	int userId = 123'456'789;
+	int key = (userId % 1000); // 0 ~ 999 = 1000개
+
+	users[key] = User{ userId, "AAA"};
+
+	int user2Id = 789;
+	int key2 = (user2Id % 1000);
+
+	users[key] = User{ user2Id, "BBB" };
+
+	// 숫자가 너무 크다. => 쪼개겠다. bucket [        ]
+
+	// 해결하기 위한 방법들을 생각을 했어야 한다.
+	// 이미 데이터가 들어 잇네? 넣지 않고 +1 그 다음에 자리가 있나? 선형 조사법 
+	// hash(key) + n^2  // ->									이차 조사법		   
+
+	// 체이닝 : 중복된 데이터가 있으면? 연속적으로 데이터를 이어가겠다.
+
+}
 
 
+// 789  {   ["AAA] -> [789, "BBB"]                          }
+// 이 데이터들 중에서 아이디가 AAA인 녀석을 찾아라?
+// 함수 -> 중복이 안되도록 하는 엄청나게 어썸한 코드. -> prime number..  제곱 수 ...  digit ...  
 
-class BinarySearchTree
+void HashChaining()
 {
-public:
-
-	void Print()
+	struct User
 	{
-		Print(_root, 10, 0);
-	}
+		int id = 0;
+		string name;
+	};
 
-	void insert(int key)
+	vector<vector<User>> users;
+
+	users.resize(1000);
+
+	int userId = 123'456'789;
+	int key = (userId % 1000); // 0 ~ 999 = 1000개
+	int user2Id = 789;
+	int key2 = (user2Id % 1000);
+
+
+	users[key].push_back(User{ userId, "AAA" });
+	users[key2].push_back(User{ user2Id, "BBB" });
+
+	for (User& user : users[key])
 	{
-		// 첫번째 노드가 삽입됏을 때
-		Node* newNode = new Node();
-		newNode->key = key;
-
-		if (_root == nullptr)
+		if (user.id == user2Id)
 		{
-			_root = newNode;
-			return;
+			cout << user.name << endl;
 		}
-
-		Node* node = _root;
-		Node* parent = nullptr;
-
-		while (node)  // 리프노드가 아니면 계속 반복해라.
-		{
-			parent = node;
-			if (key < node->key)
-				node = node->left;
-			else
-				node = node->right;
-		}
-
-		newNode->parent = parent;
-
-		if (key < parent->key)
-			parent->left = newNode;
-		else
-			parent->right = newNode;
-
 	}
 
+}
 
-	Node* find(int key)
-	{
-		return find(_root, key);
-	}
+// struct User - id value - id * 100
 
-private:
-	Node* find(Node* node, int key)
-	{
-		if (node == nullptr || key == node->key)
-			return node;
+// map -> O(n)
+// Hash Table -> 탐색 시간 O(1)
+// 공간을 사용해서 시간을 매우 추구한 버전이다.
 
-		if (key < node->key)
-			return find(node->left, key);
-		else
-			return find(node->right, key);
-	}
-
-	void Print(Node* node, int x, int y)
-	{
-		if (node == nullptr)
-			return;
-
-		SetCursorPosition(x, y);
-
-		cout << node->key;
-		Print(node->left, x - (5 / (y + 1)), y + 1);
-		Print(node->right, x + (5 / (y + 1)), y + 1);
-	}
-
-private:
-	Node* _root = nullptr;
-};
+// 로딩.. 데이터 초기화
+// 게임 플레이 => Database 99 -> 123 -> 10 
+// 99 <-  insert [10]
+// n << 아이템 업데이트. 1만. << 게임 렉이 걸려요
+// map< >  -  hash table< >	STL Cpp== unordered_map , C# == Dictionary
 
 
-// 탐색 -> BST [트리 불균형] -> N  -> 균형 잡아주는 알고리즘 실용버전.
-// logn 탐색으로 찾을 수 있다.
-// map << find logn찾을 수 있다.
-
-// Hash Table - 
-// vector, unorderd_map
+// vector, map, hash table << data table;
 
 int main()
 {
-	BinarySearchTree bst;
+	//Table();
+	//Hash();
+	//HashChaining();
 
-	bst.insert(20);
-	bst.insert(10);
-	bst.insert(30);
-	bst.insert(50);
-	bst.insert(70);
-	bst.insert(90);
-	bst.insert(110);
-	bst.insert(130);
-	bst.insert(150);
+	unordered_map<int, string> users;
 
+	users.insert(make_pair(123'456'789, "AAA"));
+	users.insert(make_pair(789, "BBB"));
 
-	bst.Print();
-
-	Node* findNode = bst.find(10);
-
-
-	while (true)
-	{
-
-	}
-
+	cout << users[789] << endl;
 }
 
 
